faccb1f6d6e3967e833cffd24e041fcdd0ebd447
zainaly0 <zaidk4076@gmail.com>
2024-06-28 10:25:38 +0530

    chat app controller code

--- app/Livewire/ChatComponent.php
+++ app/Livewire/ChatComponent.php
@@ -4,17 +4,60 @@ namespace App\Livewire;
 
 use Livewire\Component;
 use App\Models\User;
+use App\Models\Message;
 
 class ChatComponent extends Component
 {
     public $user;
+    public $sender_id;
+    public $receiver_id;
+    public $message = '';
+    public $messages = []; 
     public function render()
     {
         return view('livewire.chat-component');
     }
     
     public function mount($user_id){
+        $this->sender_id = auth()->user()->id;
+        $this->receiver_id = $user_id;
         $this->user = User::where('id', $user_id)->first();
+
+        $messages = Message::where(function($query){
+            $query->where('sender_id', $this->sender_id)
+                  ->where('receiver_id', $this->receiver_id);
+        })->orWhere(function($query){
+            $query->where('sender_id', $this->receiver_id)
+                  ->where('receiver_id', $this->sender_id);
+        })
+        ->with('sender:id,name', 'receiver:id,name')
+        ->get();
+
+        // dd($messages->toArray());
+        foreach($messages as $message){
+            $this->appendChatMessage($message);
+        }
+
+    }
+
+    public function sendMessage(){
+        $chatMessage = new Message();
+        $chatMessage->sender_id = $this->sender_id;
+        $chatMessage->receiver_id = $this->receiver_id;
+        $chatMessage->message = $this->message;
+        $chatMessage->save();
+
+        $this->message = '';
+    }
+
+
+    public function appendChatMessage($message){
+        $this->messages[] =[
+            'id' => $message->id,
+            'message' => $message->message,
+            'sender' => $message->sender->name,
+            'receiver' => $message->receiver->name,
+        ];
 
     }
 }
--- app/Models/Message.php
+++ app/Models/Message.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class Message extends Model
 {
     use HasFactory;
+    public function sender(){
+        return $this->belongsTo(User::class, 'sender_id');
+    }
+
+    public function receiver(){
+        return $this->belongsTo(User::class, 'receiver_id');
+    }
+
 }
